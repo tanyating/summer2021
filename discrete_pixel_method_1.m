9af7ff100b779c2cf67f493ef322ccd54a931932
Tanya <yw3087@nyu.edu>
2021-06-19 19:01:50 -0400

    changed discrete_pixel_method_1

--- discrete_pixel_method_1.m
+++ discrete_pixel_method_1.m
@@ -1,62 +1,105 @@
-addpath('utils')
+addpath('utils');
+clf;
 
 Rs = [0, pi/2, pi, pi*3/2]; %4 rotations
-N = 10;
-m = [1 2 0; 0 10 0];
-[q,p] = size(m); %molecule length and width
+N = 2; %N grids
+p = 2; %molecule length (p>q)
+q = 1; %molecule width
+mol = rand(q,p).*randi(10,q,p); %random molecules in 2D
 ts = 0:N-p; %N-p+1 translations
 A = zeros(N-p+1,4,N); %store all a_{t,R} (index:translation, rotation, vector)
 
-%construct a_{t,R} based on m
+%construct a_{t,R} based on mol
 for i=0:N-p
     for j=1:4
-        tmp=sum(rot90(m,j-1),1);
+        tmp=sum(rot90(mol,j-1),1);
         A(1+i,j,1+i:i+length(tmp)) = tmp;
     end
 end
 
-sigma = 0.1;
+sigmas = 0.1:0.01:1; %noise
+fps = zeros(size(sigmas));
+fns = zeros(size(sigmas));
 
-a = zeros(1,N);
-a(1,:) = A(5+1,1,:);
-y = mvnrnd(a,sigma^2*eye(N),1); %given vector with random noise sigma
+sigma_show = 1;
 
-% (1) minimize |y-a_{t,R}|
-min_i = 0;
-min_j = 1;
-a = zeros(1,length(A(1,1,:)));
-a(1,:) = A(1,1,:);
-cur_min= norm(y-a);
-for i=0:N-p
-    for j=1:4
-        a = zeros(1,length(A(i+1,j,:)));
-        a(1,:) = A(i+1,j,:);
-        if (norm(y-a) < cur_min)
-            min_i = i;
-            min_j = j;
-            cur_min= norm(y-a);
+for l=1:length(sigmas)
+    sigma = sigmas(l);
+    cov = sigma^2.*eye(N);
+    
+    m = 10000; %number of random examples
+    y = mvnrnd(zeros(N,1), cov, m); %add noise to each example
+    truelabel = zeros(m,1);
+    filter = rand(m,1); %uniform distribution 
+    p_0 = 0.5; %prior for no signal
+    tmp = (1-p_0)/(4*(N-p+1)); %equally likely for all classes with signal
+%     tmp = 1/(4*(N-p+1)+1);
+    truelabel(filter>p_0) = 1;
+%     truelabel(filter>tmp) = 1;
+    
+    % plot no signal class (guassian blobs) in 2D
+    if (N==2 && sigma==sigma_show)
+        plot(y((truelabel==0),1),y((truelabel==0),2),'.','Markersize',10);
+        hold on;
+    end
+
+    %assign each random vector with signal based on true label
+    a = zeros(1,N);
+    k=0;
+    for i=0:N-p
+        for j=1:4
+            a(1,:) = A(i+1,j,:); %a_{t,R}
+            y(filter>(p_0+k*tmp) & filter<=(p_0+(k+1)*tmp), :) = y(filter>(p_0+k*tmp) & filter<=(p_0+(k+1)*tmp), :) + a;
+            
+            % plot signal classes (guassian blobs) in 2D
+            if (N==2 && sigma==sigma_show)
+                plot(y(filter>(p_0+k*tmp) & filter<=(p_0+(k+1)*tmp),1),y(filter>(p_0+k*tmp) & filter<=(p_0+(k+1)*tmp),2),'.','Markersize',10);
+            end
+            
+            k = k+1;
         end
     end
-end
 
-fprintf('Min norm search results in translation %d and rotation %.4f\n', min_i, Rs(min_j));
 
-% (2) maximize likelihood (multivariate Gaussian)
-max_i = 0;
-max_j = 1;
-a = zeros(1,length(A(1,1,:)));
-a(1,:) = A(i,1,:);
-cur_max = mvnpdf(y,a,sigma^2*eye(N));
-for i=0:N-p
-    for j=1:4
-        a = zeros(1,length(A(i+1,j,:)));
-        a(1,:) = A(i+1,j,:);
-        if (mvnpdf(y,a,sigma^2*eye(N)) > cur_max)
-            max_i = i;
-            max_j = j;
-            cur_max = mvnpdf(y,a,sigma^2*eye(N));
+    % predict labels by minimizing distance (norm)
+    predlabel = zeros(m,1);
+
+    cur_min= sum(y.^2,2); %distance to origin (no signal)
+    for i=0:N-p
+        for j=1:4
+            a(1,:) = A(i+1,j,:);
+            norm_vec = sum((y-a).^2,2); %distance to current a_{t,R}
+            predlabel(norm_vec<cur_min) = 1;
+            cur_min(norm_vec<cur_min) = norm_vec(norm_vec<cur_min);
         end
     end
+
+    % fprintf('Min norm search results in translation %d and rotation %.4f\n', min_i, Rs(min_j));
+
+    fp = sum((truelabel-predlabel)==-1)/sum(truelabel==0);
+    fn = sum((truelabel-predlabel)==1)/sum(truelabel==1);
+    
+    fps(l) = fp;
+    fns(l) = fn;
+    
 end
 
-fprintf('Max likelihood search results in translation %d and rotation %.4f\n', max_i, Rs(max_j));
+tt = min(sigmas):0.01:max(sigmas);
+% b = norm(a)/2+tt.^2.*log(p_0/p_a)./norm(a); % decision boundary for x.a / ||a||
+
+%plot fp
+figure(2);
+% plot(tt, 1/2-1/2*erf(b./(sqrt(2).*tt)), '-', 'Linewidth', 2);
+hold on;
+plot(sigmas, fps, '.', 'Markersize', 10);
+
+%plot fn
+% figure(3);
+hold on;
+% plot(tt, 1/2-1/2*erf(-(b-norm(a))./(sqrt(2).*tt)), '-', 'Linewidth', 2);
+plot(sigmas, fns, '.', 'Markersize', 10);
+xlabel('\sigma');
+% legend('expexted fp', 'actual fp', 'expexted fn', 'actual fn');
+legend('actual fp','actual fn');
+% vline(sigma_show,'k:','sigma shown in fig.1');
+hold off;
